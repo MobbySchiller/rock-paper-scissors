e69c54aadf1c02d0683418a1fb36fa6c359a392e
MobbySchiller <kb.burzynski@gmail.com>
2022-12-30 01:37:42 +0100

    add game logic

--- src/ts/Game.ts
+++ src/ts/Game.ts
@@ -1,12 +1,52 @@
 import { rules } from './Rules'
+import { options } from './options'
 
 class Game {
-    constructor() {
+    selectSection: HTMLElement | null
+    duelSection: HTMLElement | null
+    options: NodeListOf<HTMLElement>
+    playerPick: HTMLElement | null
+    playerPickInside: HTMLElement | null
 
+    constructor() {
+        this.selectSection = document.querySelector('#select-section')
+        this.duelSection = document.querySelector('#duel-section')
+        this.options = document.querySelectorAll('[data-option]')
+        this.playerPick = document.querySelector('#player-pick')
+        this.playerPickInside = document.querySelector('#player-pick-inside')
     }
 
     init() {
         rules.init()
+        this.options.forEach(option => option.addEventListener('click', (e) => this.selectOption(e)))
+    }
+
+    selectOption(e) {
+        const id: number = Number(e.target.dataset.option)
+        const userPick: string = options[id].name
+        const aiPick: string = this.aiPick()
+        this.selectSection?.classList.remove('select--active')
+        this.duelSection?.classList.add('duel--active')
+        this.playerPick?.classList.add(`pick__option--${userPick}`)
+        this.playerPickInside?.classList.add(`pick__option-inside--${userPick}`)
+        console.log(userPick, aiPick)
+        this.checkResult(userPick, aiPick)
+    }
+
+    aiPick() {
+        const id: number = Math.floor(Math.random() * options.length)
+        const pick: string = options[id].name
+        return pick
+    }
+
+    checkResult(userPick: string, aiPick: string) {
+        if ((userPick === 'paper' && aiPick === 'rock') || (userPick === 'rock' && aiPick === 'scissors') || (userPick === 'scissors' && aiPick === 'paper')) {
+            console.log('win')
+        } else if (userPick === aiPick) {
+            console.log('draw')
+        } else {
+            console.log('lose')
+        }
     }
 }
 
---
+++ src/ts/options.ts
@@ -0,0 +1,10 @@
+interface Options {
+    id: number
+    name: string
+}
+
+export const options: Options[] = [
+    { id: 0, name: 'rock' },
+    { id: 1, name: 'paper' },
+    { id: 2, name: 'scissors' },
+]
