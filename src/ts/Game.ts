0b775930d75fd00ac13424c86b92500ee4e1e4d7
MobbySchiller <kb.burzynski@gmail.com>
2023-01-01 22:50:39 +0100

    improve game logic

--- src/ts/Game.ts
+++ src/ts/Game.ts
@@ -5,47 +5,107 @@ class Game {
     selectSection: HTMLElement | null
     duelSection: HTMLElement | null
     options: NodeListOf<HTMLElement>
-    playerPick: HTMLElement | null
-    playerPickInside: HTMLElement | null
+    userPick: HTMLElement | null
+    userPickInside: HTMLElement | null
+    aiPick: HTMLElement | null
+    aiPickInside: HTMLElement | null
+    duelResult: HTMLElement | null
+    duelResultSign: HTMLElement | null
+    playAgainBtn: HTMLButtonElement | null
+    picks: {
+        user: string,
+        ai: string
+    }
 
     constructor() {
         this.selectSection = document.querySelector('#select-section')
         this.duelSection = document.querySelector('#duel-section')
         this.options = document.querySelectorAll('[data-option]')
-        this.playerPick = document.querySelector('#player-pick')
-        this.playerPickInside = document.querySelector('#player-pick-inside')
+        this.userPick = document.querySelector('#user-pick')
+        this.userPickInside = document.querySelector('#user-pick-inside')
+        this.aiPick = document.querySelector('#ai-pick')
+        this.aiPickInside = document.querySelector('#ai-pick-inside')
+        this.duelResult = document.querySelector('#duel-result')
+        this.duelResultSign = document.querySelector('#duel-result-sign')
+        this.playAgainBtn = document.querySelector('#play-again-btn')
+        this.picks = {
+            user: '',
+            ai: ''
+        }
     }
 
     init() {
         rules.init()
         this.options.forEach(option => option.addEventListener('click', (e) => this.selectOption(e)))
+        this.playAgainBtn?.addEventListener('click', () => this.playAgain())
     }
 
     selectOption(e) {
         const id: number = Number(e.target.dataset.option)
-        const userPick: string = options[id].name
-        const aiPick: string = this.aiPick()
-        this.selectSection?.classList.remove('select--active')
-        this.duelSection?.classList.add('duel--active')
-        this.playerPick?.classList.add(`pick__option--${userPick}`)
-        this.playerPickInside?.classList.add(`pick__option-inside--${userPick}`)
-        console.log(userPick, aiPick)
-        this.checkResult(userPick, aiPick)
+        this.picks.user = options[id].name
+        this.contest()
     }
 
-    aiPick() {
+    contest() {
+        if (!this.picks.user) return
+        this.aiSelection()
+        this.changeView()
+        this.toggleUserPick()
+        setTimeout(() => this.toggleAiPick(), 1200)
+        setTimeout(() => this.displayResult(), 1800)
+        this.checkResult(this.picks.user, this.picks.ai)
+    }
+
+    aiSelection() {
         const id: number = Math.floor(Math.random() * options.length)
         const pick: string = options[id].name
-        return pick
+        this.picks.ai = pick
+    }
+
+    changeView() {
+        this.selectSection?.classList.toggle('select--active')
+        this.duelSection?.classList.toggle('duel--active')
+    }
+
+    toggleUserPick() {
+        this.userPick?.classList.toggle(`pick__option--${this.picks.user}`)
+        this.userPickInside?.classList.toggle(`pick__option-inside--${this.picks.user}`)
+    }
+
+    toggleAiPick() {
+        this.aiPick?.classList.toggle(`pick__option--${this.picks.ai}`)
+        this.aiPickInside?.classList.toggle(`pick__option-inside`)
+        this.aiPickInside?.classList.toggle(`pick__option-inside--${this.picks.ai}`)
+    }
+
+    displayResult() {
+        this.setResultSign()
+        this.duelResult?.classList.toggle('duel__result--active')
+    }
+
+    setResultSign() {
+        const result = this.checkResult(this.picks.user, this.picks.ai)
+        this.duelResultSign.textContent = `you ${result}!`
     }
 
     checkResult(userPick: string, aiPick: string) {
         if ((userPick === 'paper' && aiPick === 'rock') || (userPick === 'rock' && aiPick === 'scissors') || (userPick === 'scissors' && aiPick === 'paper')) {
-            console.log('win')
+            return 'win'
         } else if (userPick === aiPick) {
-            console.log('draw')
+            return 'tie'
         } else {
-            console.log('lose')
+            return 'lose'
+        }
+    }
+
+    playAgain() {
+        this.changeView()
+        this.toggleUserPick()
+        this.toggleAiPick()
+        this.displayResult()
+        this.picks = {
+            user: '',
+            ai: ''
         }
     }
 }
